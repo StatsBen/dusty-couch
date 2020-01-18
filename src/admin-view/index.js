2728ee12911c47dd8697308c31c0a6669d3146e7
Ben Clark <bclark@avalancheassociation.ca>
2020-01-18 12:42:05 -0800

    Initialized a couple of simple React classes for the two pages I plan to implement

---
+++ src/admin-view/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class AdminView extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>Welcome to the admin portal for the dusty couch</h4>
+      </div>
+    );
+  }
+}
+
+export default AdminView;
---
+++ src/user-view/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class UserView extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>Welcome to the main page for the Dusty Couch</h4>
+      </div>
+    );
+  }
+}
+
+export default UserView;
