4650cee65a63625e1cdebc1b23b013c793e9b266
Ben Clark <bclark@avalancheassociation.ca>
2020-01-20 13:16:55 -0800

    Created a utils folder and added a reusable UI components folder and file

---
+++ src/utils/reusable-components/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const BaseButton = styled.button`
+  padding: 10px;
+  border-radius: 5px;
+`;
+
+export const AuthButton = styled(BaseButton)`
+  background: red;
+  color: yellow;
+`;
