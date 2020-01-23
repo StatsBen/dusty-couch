2959b4c98b36c8319a8d6df991256018c31216ee
Ben Clark <bclark@avalancheassociation.ca>
2020-01-23 13:18:36 -0800

    Put all the styled form input components that used to be in the BookingForm into their own file (it was cluttered)

--- src/booking-form/BookingForm.js
+++ src/booking-form/BookingForm.js
@@ -1,80 +1,13 @@
 import React, { useState } from "react";
-import styled from "styled-components";
-import { BaseInput, BaseLabel } from "../utils/reusable-components";
-import colours from "../styles/colours";
-import fonts from "../styles/fonts";
-
-const BookingBackdrop = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  background: rgba(0, 0, 0, 0.8);
-  z-index: 999;
-`;
-
-const BookingContainer = styled.div`
-  position: absolute;
-  width: calc(90% - 40px);
-  left: 5%;
-  top: 100px;
-  padding: 20px;
-  font-family: ${fonts.primary}, sans-serif;
-  color: ${colours.paragraph};
-  background: ${colours.background};
-  z-index: 1000;
-  border-radius: 10px;
-`;
-
-const BookingHeader = styled.h1`
-  color: ${colours.headline};
-`;
-
-const BookingLabel = styled(BaseLabel)`
-  display: block;
-  position: relative;
-  float: left;
-  margin: 5px;
-  font-weight: 700;
-  height: auto;
-  color: ${colours.paragraph};
-  white-space: nowrap;
-`;
-
-const BookingTextInput = styled(BaseInput)`
-  background: white;
-`;
-
-const BookingLongTextInput = styled.textarea`
-  padding: 5px;
-  border-radius: 5px;
-  border: thin solid #888888;
-  background: white;
-`;
-
-const SubmitButton = styled.input`
-  display: block;
-  position: relative;
-  float: right;
-  clear: both;
-  margin: 15px;
-  padding: 8px;
-  font-size: 1.2em;
-  font-weight: 700;
-  color: ${colours.buttonText};
-  background: ${colours.button};
-  border: none;
-  border-radius: 5px;
-  &:hover {
-    border: thin solid white;
-    cursor: pointer;
-    box-shadow: 1px 1px 3px #666666;
-  }
-  &:focus {
-    border: none;
-  }
-`;
+import {
+  BookingBackdrop,
+  BookingContainer,
+  BookingHeader,
+  BookingLabel,
+  BookingTextInput,
+  BookingLongTextInput,
+  SubmitButton
+} from "./BookingFormSubComponents";
 
 const BookingForm = props => {
   const [arrivalDate, updateArrivalDate] = useState("");
---
+++ src/booking-form/BookingFormSubComponents.js
@@ -0,0 +1,76 @@
+import styled from "styled-components";
+import { BaseInput, BaseLabel } from "../utils/reusable-components";
+import colours from "../styles/colours";
+import fonts from "../styles/fonts";
+
+export const BookingBackdrop = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.8);
+  z-index: 999;
+`;
+
+export const BookingContainer = styled.div`
+  position: absolute;
+  width: calc(90% - 40px);
+  left: 5%;
+  top: 100px;
+  padding: 20px;
+  font-family: ${fonts.primary}, sans-serif;
+  color: ${colours.paragraph};
+  background: ${colours.background};
+  z-index: 1000;
+  border-radius: 10px;
+`;
+
+export const BookingHeader = styled.h1`
+  color: ${colours.headline};
+`;
+
+export const BookingLabel = styled(BaseLabel)`
+  display: block;
+  position: relative;
+  float: left;
+  margin: 5px;
+  font-weight: 700;
+  height: auto;
+  color: ${colours.paragraph};
+  white-space: nowrap;
+`;
+
+export const BookingTextInput = styled(BaseInput)`
+  background: white;
+`;
+
+export const BookingLongTextInput = styled.textarea`
+  padding: 5px;
+  border-radius: 5px;
+  border: thin solid #888888;
+  background: white;
+`;
+
+export const SubmitButton = styled.input`
+  display: block;
+  position: relative;
+  float: right;
+  clear: both;
+  margin: 15px;
+  padding: 8px;
+  font-size: 1.2em;
+  font-weight: 700;
+  color: ${colours.buttonText};
+  background: ${colours.button};
+  border: none;
+  border-radius: 5px;
+  &:hover {
+    border: thin solid white;
+    cursor: pointer;
+    box-shadow: 1px 1px 3px #666666;
+  }
+  &:focus {
+    border: none;
+  }
+`;
