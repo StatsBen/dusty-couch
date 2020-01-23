1de3c94675ebb1d88886b8d5200050b8b3bd5258
Ben Clark <bclark@avalancheassociation.ca>
2020-01-22 21:44:12 -0800

    Added some basic styles systems with a colour pallet I liked on a website...

---
+++ src/styles/colours.js
@@ -0,0 +1,12 @@
+export default {
+  black: "#111111",
+  background: "#fffffe",
+  button: "#00ebc7",
+  buttonText: "#00214d",
+  headline: "#00214d",
+  highlight: "#00ebc7",
+  paragraph: "#1b2d45",
+  secondary: "#ff5470",
+  stroke: "#00214d",
+  tertiary: "#fde24f"
+};
---
+++ src/styles/fonts.js
@@ -0,0 +1,11 @@
+var WebFont = require("webfontloader");
+
+WebFont.load({
+  google: {
+    families: ["IBM Plex Sans"]
+  }
+});
+
+export default {
+  primary: "IBM Plex Sans"
+};
