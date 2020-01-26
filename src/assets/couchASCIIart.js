22ed0c15adc24fd7561ef96335aef6b9d91088fc
Ben Clark <bclark@avalancheassociation.ca>
2020-01-26 15:43:18 -0800

    Added min-width to couch art to prevent graphics from breaking

--- src/assets/couchASCIIart.js
+++ src/assets/couchASCIIart.js
@@ -3,6 +3,9 @@ import styled from "styled-components";
 
 const CoochContainer = styled.p`
   font-family: "Courier New", Courier, monospace;
+  font-weight: 900;
+  font-size: 1.3em;
+  min-width: 12em;
 `;
 
 const cooch = () => {
