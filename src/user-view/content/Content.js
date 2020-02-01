bbd64c2b95a175214d806be483c76338f70bb7a6
Ben Clark <bclark@avalancheassociation.ca>
2020-02-01 11:18:48 -0800

    Included the first Sam graphic and it looks kinda good, methinks :p

--- src/user-view/content/Content.js
+++ src/user-view/content/Content.js
@@ -11,7 +11,6 @@ const ContentContainer = styled.div`
 const GiantHeader = styled.h1`
   position: relative;
   margin-bottom: -0.3em;
-  font-size: 3em;
   z-index: 10;
 `;
 
@@ -35,15 +34,39 @@ const Caption = styled.div`
   font-size: 0.9em;
 `;
 
+const TextSection = styled.div`
+  position: relative;
+  width: 100%;
+  clear: both;
+	padding; 0;
+  text-align: center;
+  & > p {
+    display: inline-block;
+    max-width: 600px;
+    text-align: left;
+		margin: 0;
+		padding: 5px;
+		border: thin solid ${colours.tertiary};
+		transform: rotate(3deg);
+		border-radius: 3px;
+  }
+`;
+
 const Content = () => {
   return (
     <ContentContainer>
       <GiantHeader>WELCOME TO:</GiantHeader>
       <BigCoochGraphic src={samGraphics["DUSTY_COUCH"]} />
       <Caption>
-        Actually please don't call... That's annoying <br />
+        Actually please don&apos;t call... That&apos;s annoying <br />
         (Art by Sam Bowerman)
       </Caption>
+      <TextSection>
+        <p>
+          Ever wanted to live with your best friend for a couple nights? Come
+          stay on THE DUSTY COUCH!!
+        </p>
+      </TextSection>
     </ContentContainer>
   );
 };
